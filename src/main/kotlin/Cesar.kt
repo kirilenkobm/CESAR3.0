0d2d28e28ebaceecb5947f3631b19ad4156a5fec
Bogdan Kirilenko <kirilenkobm@gmail.com>
2023-08-01 13:23:06 +0200

    temporarily added previous CESAR2.0 code for reference

--- src/main/kotlin/Cesar.kt
+++ src/main/kotlin/Cesar.kt
@@ -1,10 +1,17 @@
 // Contains the main application logic and is the entry point of the program.
 
-
 object Cesar {
+    private val fastaParser by lazy { FastaParser() }
+    private val viterbi by lazy { Viterbi() }
 
-    fun run(args: Array<String>) {
-        val cesarArgs = CesarArgsParser.parse(args)
-        println(cesarArgs)
+    suspend fun run(args: Array<String>) {
+        val arguments = CesarArgsParser.parse(args)
+        val fasta = fastaParser.parse(arguments.fastaFile)
+        val profiles = fasta.references.map { Profile(it) }
+        val hmm = HMM(profiles)
+        val alignments = fasta.queries.map { query ->
+            async { viterbi.run(hmm, query) }
+        }
+        alignments.awaitAll().forEach { it.print() }
     }
 }
